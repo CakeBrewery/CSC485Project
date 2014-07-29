753f959d4e9316482ccc2b84b7a42a0389427b89
Samuel N <samuel.e.navarrete@gmail.com>
2014-07-28 17:06:26 -0700

    added playermatch class

---
+++ PlayerMatch.java
@@ -0,0 +1,11 @@
+public class PlayerMatch{
+	public int kills = 0; 
+    public int deaths = 0; 
+    public int assists = 0; 
+    public int kda = 0;
+    public int kd = 0;
+    public int lasthit = 0; 
+    public int denies = 0; 
+    public int gpm = 0; 
+    public int xpm = 0; 
+}
