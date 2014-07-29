cc3f3f28ab10ae354cee4ea6d6f781235b665b62
Samuel N <samuel.e.navarrete@gmail.com>
2014-07-29 15:59:27 -0700

    lp tests

--- matchmaker.java
+++ matchmaker.java
@@ -1,4 +1,7 @@
 import java.util.*;
+import scpsolver.lpsolver.*;
+import scpsolver.problems.*;
+import scpsolver.constraints.*;
 
 public class matchmaker {
 
@@ -61,6 +64,16 @@ public class matchmaker {
       }catch (NumberFormatException e) {
         System.out.println("Incorrect usage!");
       }
+
+      LinearProgram lp = new LinearProgram(new double[]{5.0,10.0}); 
+      lp.addConstraint(new LinearBiggerThanEqualsConstraint(new double[]{3.0,1.0}, 8.0, "c1")); 
+      lp.addConstraint(new LinearBiggerThanEqualsConstraint(new double[]{0.0,4.0}, 4.0, "c2")); 
+      lp.addConstraint(new LinearSmallerThanEqualsConstraint(new double[]{2.0,0.0}, 2.0, "c3")); 
+      lp.setMinProblem(true); 
+      LinearProgramSolver solver  = SolverFactory.newDefault(); 
+      double[] sol = solver.solve(lp);
+
+
 
 
 
